f03bd5fa082694ba2fdc60d2f775c1cf1a8af08a
shawn <shawncaceres10@gmail.com>
2021-11-05 06:57:48 -0500

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
+import margarita from '../images/margarita.jpg'
+import bloodymary from '../images/bloodymary.jpg'
+import mimosa from '../images/mimosa.jpg'
+import oldfashioned from '../images/oldfashioned.jpg'
+import manhattan from '../images/manhattan.jpg'
+
 const Header = () => {
   return (  
     <>
       <h1 className='header'>The Drinktionary</h1>
+
+      <div className='pop-drink-wrapper'>
+      <div>Popular Cocktails</div>
+      <div className="popular-drink-container">
+        
+        
+        <img src={margarita} alt="margarita" />
+        <img src={bloodymary} alt="bloody mary" />
+        <img src={mimosa} alt="bloody mary" />
+        <img src={oldfashioned} alt="bloody mary" />
+        <img src={manhattan} alt="bloody mary" />
+
+      </div>
+      </div>
+      
     </>
   );
 }
