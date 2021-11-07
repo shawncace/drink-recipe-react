fd24abb274407982ab2719a669a1d34f620d9da6
shawn <shawncaceres10@gmail.com>
2021-11-07 08:41:16 -0600

    working api search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-
+import {useState} from 'react'
 
 const Search = () => {
+  const [searchValue, setSearchValue]=useState('')
+
+  const handleClick=() =>{
+    getDrink(searchValue)
+  }
+
+  const getDrink=async(searchValue)=>{
+    const API_URL=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+
+    try{
+      const response = await fetch(API_URL)
+      const data = await response.json()
+        console.log(data.drinks[0].strDrink)
+        
+    } catch(error){
+      console.log(error)
+    }    
+  };
+  
   return (  
     <div className='recipe-container'>
-
+      <input onChange={(event)=>setSearchValue(event.target.value)}/>
+      <button onClick={handleClick}>Go</button>
     </div>
   );
 }
