71c685bb0b0f49a92525a081476d7dbcc3f0c08e
shawn <shawncaceres10@gmail.com>
2021-11-09 12:58:58 -0600

    search container updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,13 @@ import {useState} from 'react'
 const Search = () => {
   const [searchValue, setSearchValue]=useState('')
 
+  const[drink, setDrink]=useState({})
+
   const handleClick=() =>{
     if(searchValue === ''){
       alert('gg')
     }
+    
     getDrink(searchValue)
   }
 
@@ -16,9 +19,14 @@ const Search = () => {
         alert('gg')
       }
 
+
       getDrink(searchValue)
      
     }
+  }
+
+  const handleRandomClick = ()=>{
+    getRandomDrink()
   }
 
   const getDrink=async(searchValue)=>{
@@ -27,19 +35,62 @@ const Search = () => {
     try{
       const response = await fetch(API_URL)
       const data = await response.json()
+
+      
         console.log(data.drinks[0].strDrink)
+        setDrink(data.drinks[0])
+        
         
     } catch(error){
       console.log(error)
     }    
   };
+
+  const getRandomDrink = async()=>{
+    const API_URL='https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+    try{
+      const response = await fetch(API_URL)
+      const data = await response.json()
+
+      
+        console.log(data.drinks[0].strDrink)
+        setDrink(data.drinks[0])
+        
+        
+    } catch(error){
+      console.log(error)
+    }
+  }
   
   return (  
-    <div className='recipe-container'>
-      <input onChange={(event)=>setSearchValue(event.target.value)}
-             onKeyPress={handleKeypress}/>
-      <button onClick={handleClick}>Go</button>
-    </div>
+    <>
+      <div className='section-title'>Search for a Drink
+      </div>
+
+      {/* <div className='recipe-container'>
+        <div className="find-drink-container">
+          <input onChange={(event)=>setSearchValue(event.target.value)}
+                onKeyPress={handleKeypress}
+                placeholder='Name'/>
+          <button onClick={handleClick}>GO</button>
+          </div>
+
+          <button className='random-bev-btn' onClick={handleRandomClick}>Random Beverage</button>
+      </div> */}
+
+      <div className='recipe-container-two'>
+        <div className="find-drink-container">
+          <img src={drink.strDrinkThumb} alt=''/>
+          <input onChange={(event)=>setSearchValue(event.target.value)}
+                onKeyPress={handleKeypress}
+                placeholder='Name'/>
+          <button onClick={handleClick}>GO</button>
+          </div>
+
+          <button className='random-bev-btn' onClick={handleRandomClick}>Random Beverage</button>
+      </div>
+    </>
   );
 }
  
